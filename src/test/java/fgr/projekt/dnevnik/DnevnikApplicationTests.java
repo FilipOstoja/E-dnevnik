8d9e6fd848c4f5ccc25fa3e3f8b56b724175a64f
[FilipOstoja] <[dev.filip.ostojic@gmail.com]>
2023-01-23 01:46:46 +0100

    Initial commit

---
+++ src/test/java/fgr/projekt/dnevnik/DnevnikApplicationTests.java
@@ -0,0 +1,13 @@
+package fgr.projekt.dnevnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DnevnikApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
